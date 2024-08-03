a62d12e0704ba05717608066e7430fc8e3f7371b
him <him@local>
2024-08-03 13:03:01 +0200

    clean Repo

--- src/utils/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import os
-
-def load_config(config_name):
-    config_path = os.path.join('src/configs', f'{config_name}.yaml')
-    with open(config_path, 'r') as file:
-        return yaml.safe_load(file)
-
-database_config = load_config('database_config')
